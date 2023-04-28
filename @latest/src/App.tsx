e0b1adff4b9d5e22d00cb857493632fd8995c10e
PieroMastro <pier.andres@gmail.com>
2023-04-28 02:57:38 +0000

    testing alerts

--- @latest/src/App.tsx
+++ @latest/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 // import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 // function App() {
 //   let items = ["New York", "San Francisco", "Tokio", "London", "Paris"];
@@ -21,11 +23,16 @@ import Alert from "./components/Alert";
 // }
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <>
-      <Alert>
-        <h1>Hello World</h1>
-      </Alert>
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>Hi! I'm an Alert!</Alert>
+      )}
+      <Button color="warning" onClick={() => setShowAlert(true)}>
+        My Button
+      </Button>
     </>
   );
 }
