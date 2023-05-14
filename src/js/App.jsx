d631577521aa42cb97c150c9ecabb7fd8ff9dfb1
PieroMastro <pier.andres@gmail.com>
2023-05-14 03:53:05 +0000

    testing axios delete()

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -40,13 +40,20 @@ const App = () => {
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user) => {
+    setUsers(users.filter(u => u.id !== user.id));
+  }
+
   return (
     <>
       {error && <p className="text-danger">Error: {error}</p>}
       {loading === true && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group" >
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="list-group-item d-flex justify-content-between" >
+            {user.name}
+            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+          </li>
         ))}
       </ul>
     </>
